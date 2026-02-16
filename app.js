366bf341c0e1605be00b07a45a822880b721780f
shrestha035 <shrestha.sunil051@gmail.com>
2026-02-16 10:19:13 +0530

    Fix console error messages for API key check

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const apiKey = process.env.WEATHER_API_KEY;
 if (!apiKey) {
-  console.error("CRITICAL ERROR: No API Key found!");
-  process.exit(1);
+console.error(&quot;CRITICAL ERROR: No API Key found!&quot;);
+process.exit(1);
 }
-console.log("App is running securely.");
+console.log(&quot;App is running securely.&quot;);
